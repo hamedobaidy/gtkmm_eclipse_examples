fdce80ed26aa449a36eb96f94c2d22910409503d
hamedapps <hamedapps@gmail.com>
2015-07-03 13:23:52 +0430

    Adding gtkmm_alignment

---
+++ gtkmm_alignment/src/examplewindow.h
@@ -0,0 +1,30 @@
+/*
+ * examplewindow.h
+ *
+ *  Created on: Jul 1, 2015
+ *      Author: hamed
+ */
+
+#ifndef EXAMPLEWINDOW_H_
+#define EXAMPLEWINDOW_H_
+
+
+#include <gtkmm.h>
+
+class ExampleWindow : public Gtk::Window
+{
+public:
+  ExampleWindow();
+  virtual ~ExampleWindow();
+
+protected:
+  //Signal handlers:
+  void on_button_clicked();
+
+  //Child widgets:
+  Gtk::Alignment m_Alignment;
+  Gtk::Button m_Button;
+};
+
+
+#endif /* EXAMPLEWINDOW_H_ */
